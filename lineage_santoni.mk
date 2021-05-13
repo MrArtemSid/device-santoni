86c1a7c01c5fd85379a7b96c1d2c634fbb89a3e6
Jabiyeff <cebiyevanar@gmail.com>
2021-05-13 22:30:10 +0400

    santoni: Switch prebuilt perf blobs from LA.UM.8.6.2.r1-08600-89xx.0
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- lineage_santoni.mk
+++ lineage_santoni.mk
@@ -49,10 +49,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock \
-    vendor.qti.hardware.perf@1.0.vendor \
-    vendor.qti.hardware.perf@2.0.vendor \
-    vendor.qti.hardware.perf@2.1.vendor
+    android.hardware.power.stats@1.0-service.mock
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
